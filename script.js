0402ad738166c1b9ad429dfc63e7d74a72f24798
Mhd Risyad Asshiddiqie <mhdrisyadaashiddiqie8084@gmail.com>
2026-04-22 22:43:30 +0700

    RESET TOTAL FIX UI

--- script.js
+++ script.js
@@ -8,11 +8,7 @@ let produk=[
   {nama:"Sticker",harga:20000,kategori:"aksesoris",img:"images.jpg"}
 ];
 
-let keranjang=JSON.parse(localStorage.getItem("keranjang"))||[];
-
-function simpan(){
-  localStorage.setItem("keranjang",JSON.stringify(keranjang));
-}
+let keranjang=[];
 
 function tampilProduk(list){
   let c=document.getElementById("produk-list");
@@ -39,40 +35,16 @@ function filterKategori(k){
 }
 
 function tambahKeranjang(n,h){
-  let i=keranjang.find(x=>x.nama==n);
-  if(i)i.qty++; else keranjang.push({nama:n,harga:h,qty:1});
-  simpan(); tampilCart(); notif();
+  keranjang.push({nama:n,harga:h});
+  notif();
 }
 
-function tampilCart(){
-  let l=document.getElementById("list-keranjang");
-  l.innerHTML="";
-  let total=0;
-
-  keranjang.forEach((i,x)=>{
-    let sub=i.harga*i.qty;
-    total+=sub;
-    l.innerHTML+=`
-      <li>
-        ${i.nama}<br>
-        ${i.qty} x Rp${formatRupiah(i.harga)}<br>
-        Subtotal: Rp${formatRupiah(sub)}<br>
-        <button onclick="hapus(${x})">x</button>
-      </li>`;
-  });
-
-  document.getElementById("total").innerText=formatRupiah(total);
-  document.getElementById("cart-count").innerText=
-    keranjang.reduce((s,i)=>s+i.qty,0);
+function bukaCart(){
+  document.getElementById("cart").style.right="0";
 }
-
-function hapus(i){
-  keranjang.splice(i,1);
-  simpan(); tampilCart();
+function tutupCart(){
+  document.getElementById("cart").style.right="-300px";
 }
-
-function bukaCart(){document.getElementById("cart").style.right="0";}
-function tutupCart(){document.getElementById("cart").style.right="-400px";}
 
 function notif(){
   let n=document.getElementById("notif");
@@ -80,17 +52,4 @@ function notif(){
   setTimeout(()=>n.style.display="none",1000);
 }
 
-function checkout(){
-  let nama=document.getElementById("nama").value;
-  let alamat=document.getElementById("alamat").value;
-
-  if(!nama||!alamat) return alert("Isi data dulu!");
-
-  let text=keranjang.map(i=>`${i.nama} ${i.qty}pcs`).join("%0A");
-  let total=keranjang.reduce((s,i)=>s+i.harga*i.qty,0);
-
-  window.open(`https://wa.me/6281267798478?text=Order:%0A${text}%0ATotal:Rp${formatRupiah(total)}%0ANama:${nama}%0AAlamat:${alamat}`);
-}
-
 tampilProduk(produk);
-tampilCart();
